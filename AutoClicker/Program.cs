87cd52aec10915ecbe90b7c885dd8c9b1044d00c
YeemiRouth <yeemirouth@gmail.com>
2024-06-04 17:37:59 +1000

    repaired

--- AutoClicker/Program.cs
+++ AutoClicker/Program.cs
@@ -1,9 +1,10 @@
-﻿using AutoClicker;
+using AutoClicker;
 
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 class Program
 {
@@ -29,49 +30,84 @@ class Program
 
         Console.ReadKey();
     }
-    d
 
     private static void OnKeypress(KeyEvent @event)
     {
         if (@event.VKey != VKeyCodes.KeyHeld)
             return;
 
-        if (@event.Key == 1) //  && map.GetDown((uint)Keys.LShiftKey)
+        if (@event.Key == 1 && map.GetDown((uint)Keys.LShiftKey)) //  && map.GetDown((uint)Keys.LShiftKey)
         {
             HitPlayer();
         }
-        if (@event.Key == 2) //  && map.GetDown((uint)Keys.LShiftKey)
+        if (@event.Key == 2 && map.GetDown((uint)Keys.LShiftKey)) //  && map.GetDown((uint)Keys.LShiftKey)
         {
             PlaceBlock();
         }
+    }
+
+    public static IntPtr CursorToLParam(IntPtr Hwnd)
+    {
+        if (Hwnd == IntPtr.Zero)
+            return IntPtr.Zero;
+
+        POINT cursorPos;
+
+        if (!GetCursorPos(out cursorPos))
+            return IntPtr.Zero;
+
+        if (!ScreenToClient(Hwnd, ref cursorPos))
+            return IntPtr.Zero;
+
+        IntPtr lParam = (IntPtr)((cursorPos.Y << 16) | (cursorPos.X & 0xFFFF)); // shift into lParam
+
+        return lParam;
     }
 
     public static void HitPlayer()
     {
         IntPtr Hwnd = GetForegroundWindow();
 
-        PostMessage(Hwnd, WM_LBUTTONDOWN, 0, 0);
+        IntPtr lParam = CursorToLParam(Hwnd);
+
+        PostMessage(Hwnd, WM_LBUTTONDOWN, new IntPtr(1), lParam);
         //Thread.Sleep(15); // minimal sleep
-        PostMessage(Hwnd, WM_LBUTTONUP, 0, 0);
+        PostMessage(Hwnd, WM_LBUTTONUP, new IntPtr(1), lParam);
     }
 
     public static void PlaceBlock()
     {
         IntPtr Hwnd = GetForegroundWindow();
 
-        PostMessage(Hwnd, WM_RBUTTONDOWN, 0, 0);
+        IntPtr lParam = CursorToLParam(Hwnd);
+
+        PostMessage(Hwnd, WM_RBUTTONDOWN, new IntPtr(1), lParam);
         //Thread.Sleep(15); // minimal sleep
-        PostMessage(Hwnd, WM_RBUTTONUP, 0, 0);
+        PostMessage(Hwnd, WM_RBUTTONUP, new IntPtr(1), lParam);
     }
 
-    [DllImport("user32.dll")]
+    // SYSTEM32 SHIT
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
     static extern IntPtr GetForegroundWindow();
 
-    [DllImport("user32.dll")]
-    static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    private static extern bool GetCursorPos(out POINT lpPoint);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    private static extern bool ScreenToClient(IntPtr hWnd, ref POINT lpPoint);
 
     const int WM_LBUTTONDOWN = 0x0201;
     const int WM_LBUTTONUP = 0x0202;
     const int WM_RBUTTONDOWN = 0x0204;
     const int WM_RBUTTONUP = 0x0205;
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+    }
 }
